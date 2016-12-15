65b3c8bad669f845c6e3e75aa9575fc8a16d7071
Kunal Mehta <legoktm@member.fsf.org>
2016-12-14 20:10:06 -0800

    Add GPL file headers
    Change-Id: I74da975ef55a4a2e436166d6b7a7ed94d767f8aa

--- updateBlacklist.php
+++ updateBlacklist.php
@@ -1,7 +1,21 @@
 <?php
-
 /**
- * Reads the blacklist file and sticks it in memcache
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
  */
 
 $IP = getenv( 'MW_INSTALL_PATH' );
@@ -9,9 +23,11 @@ if ( $IP === false ) {
 	$IP = __DIR__ . '/../..';
 }
 
-// Require base maintenance class
 require_once( "$IP/maintenance/Maintenance.php" );
 
+/**
+ * Reads the blacklist file and sticks it in memcache
+ */
 class SFSBlacklistUpdate extends Maintenance {
 
 	public function execute() {
