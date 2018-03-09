bc08f777e353b0bee4e594bdf9e29e2b0ac473f6
Jayprakash12345 <0freerunning@gmail.com>
2018-03-09 17:31:17 +0000

    fix typo compatibilty to compatibility
    Change-Id: I921f5a5e8c74e96676c6edb664e21f865f4c9dd3

--- updateBlacklist.php
+++ updateBlacklist.php
@@ -1,6 +1,6 @@
 <?php
 /**
  * Alias file for renamed maintenance script.
- * Backward compatibilty for cron jobs.
+ * Backward compatibility for cron jobs.
  */
 require_once __DIR__ . '/maintenance/updateBlacklist.php';
