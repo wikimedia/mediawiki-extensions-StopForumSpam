ac523f4e6b31b00ec4485312c9acc30f8fafed7f
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-08-20 19:59:28 +0000

    Soft-deprecate updateBlacklist.php file
    File was renamed in 3e7633e to maintenance/updateBlacklist.php to match
    the custom of having maintenance scripts in their own folders. Let's
    just mention in the code more clearly that this file is deprecated and
    that it'll eventually go away.
    
    Bug: T202320
    Change-Id: I5a9c23ced6ad5bcc90ceb4f651829725cfe33198

--- updateBlacklist.php
+++ updateBlacklist.php
@@ -2,5 +2,6 @@
 /**
  * Alias file for renamed maintenance script.
  * Backward compatibility for cron jobs.
+ * @deprecated since REL1_31; please use maintenance/updateBlacklist.php instead.
  */
 require_once __DIR__ . '/maintenance/updateBlacklist.php';
