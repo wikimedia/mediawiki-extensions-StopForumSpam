7cf597cdca84ac04624d7e57a0336c7f29eee628
MarcoAurelio <maurelio@tools.wmflabs.org>
2019-03-30 20:10:47 +0000

    Follow-up I5a9c23ce: Hard-deprecate `updateBlacklist.php`
    Bug: T202320
    Change-Id: I80401a7708272f1b1def5cdd9ac4fec83370bec2

--- updateBlacklist.php
+++ updateBlacklist.php
@@ -4,4 +4,5 @@
  * Backward compatibility for cron jobs.
  * @deprecated since REL1_31; please use maintenance/updateBlacklist.php instead.
  */
+wfDeprecated( __METHOD__, '1.31' );
 require_once __DIR__ . '/maintenance/updateBlacklist.php';
