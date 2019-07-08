f109756bd7ddab0a4bfa9db7db7452f125d52a5e
Reedy <reedy@wikimedia.org>
2019-07-08 09:26:09 +0000

    Remove old entry points
    Bug: T202320
    Change-Id: Ibb71744c0426c8de0c84d8ab2a947f0451b18d37

--- updateBlacklist.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Alias file for renamed maintenance script.
- * Backward compatibility for cron jobs.
- * @deprecated since REL1_31; please use maintenance/updateBlacklist.php instead.
- */
-wfDeprecated( __METHOD__, '1.31' );
-require_once __DIR__ . '/maintenance/updateBlacklist.php';
