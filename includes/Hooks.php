5e3aab6e8be0e8effa97f9b454020230b6a70e3d
sbassett <sbassett@wikimedia.org>
2022-09-21 13:11:15 -0500

    Update StopForumSpam AbuseFilter hooks to use their own hook handler
    Bug: T318262
    Change-Id: I17dfced7b8d1fc744db61fd04ab4f65381b6defd

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -18,98 +18,29 @@
  * @file
  */
 
-// phpcs:disable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
-
 namespace MediaWiki\StopForumSpam;
 
 use Html;
 use MediaWiki\Block\DatabaseBlock;
-use MediaWiki\Extension\AbuseFilter\Hooks\AbuseFilterBuilderHook;
-use MediaWiki\Extension\AbuseFilter\Hooks\AbuseFilterComputeVariableHook;
-use MediaWiki\Extension\AbuseFilter\Hooks\AbuseFilterGenerateUserVarsHook;
-use MediaWiki\Extension\AbuseFilter\Variables\VariableHolder;
 use MediaWiki\Hook\OtherBlockLogLinkHook;
 use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\Permissions\Hook\GetUserPermissionsErrorsExpensiveHook;
-use RecentChange;
 use RequestContext;
 use Title;
 use User;
 use Wikimedia\IPUtils;
 
 class Hooks implements
-	AbuseFilterBuilderHook,
-	AbuseFilterComputeVariableHook,
-	AbuseFilterGenerateUserVarsHook,
 	GetUserPermissionsErrorsExpensiveHook,
 	OtherBlockLogLinkHook
 {
-
-	/**
-	 * Computes the sfs-blocked variable
-	 * @param string $method
-	 * @param VariableHolder $vars
-	 * @param array $parameters
-	 * @param ?string &$result
-	 * @return bool
-	 */
-	public function onAbuseFilter_computeVariable(
-		string $method, VariableHolder $vars, array $parameters, ?string &$result
-	) {
-		if ( $method === 'sfs-blocked' ) {
-			$ip = self::getIPFromUser( $parameters['user'] );
-			if ( $ip === false ) {
-				$result = false;
-			} else {
-				$result = DenyListManager::singleton()->isIpDenyListed( $ip );
-			}
-
-			return false;
-		}
-
-		return true;
-	}
-
-	/**
-	 * Load our blocked variable
-	 * @param VariableHolder $vars
-	 * @param User $user
-	 * @param ?RecentChange $rc
-	 * @return bool
-	 */
-	public function onAbuseFilter_generateUserVars( VariableHolder $vars, User $user, ?RecentChange $rc ) {
-		global $wgSFSIPListLocation;
-
-		if ( $wgSFSIPListLocation ) {
-			$vars->setLazyLoadVar( 'sfs_blocked', 'sfs-blocked', [ 'user' => $user ] );
-		}
-
-		return true;
-	}
-
-	/**
-	 * Tell AbuseFilter about our sfs-blocked variable
-	 * @param array &$builderValues
-	 * @return bool
-	 */
-	public function onAbuseFilter_builder( &$builderValues ) {
-		global $wgSFSIPListLocation;
-
-		if ( $wgSFSIPListLocation ) {
-			// Uses: 'abusefilter-edit-builder-vars-sfs-blocked'
-			$builderValues['vars']['sfs_blocked'] = 'sfs-blocked';
-		}
-
-		return true;
-	}
-
 	/**
 	 * Get an IP address for a User if possible
 	 *
 	 * @param User $user
 	 * @return bool|string IP address or false
 	 */
-	private static function getIPFromUser( User $user ) {
+	public static function getIPFromUser( User $user ) {
 		if ( $user->isAnon() ) {
 			return $user->getName();
 		}
